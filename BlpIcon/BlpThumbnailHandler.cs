977d35edc68223c0bf5dda64fa6e28b46526e880
Dramacydal <PulLumBerMal@gmail.com>
2018-08-12 21:54:49 +0300

    add blp icon previewer

---
+++ BlpIcon/BlpThumbnailHandler.cs
@@ -0,0 +1,43 @@
+﻿using SereniaBLPLib;
+using SharpShell.Attributes;
+using SharpShell.Diagnostics;
+using SharpShell.SharpIconHandler;
+using SharpShell.SharpPreviewHandler;
+using SharpShell.SharpThumbnailHandler;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlpIcon
+{
+    [ComVisible(true)]
+    [DisplayName("BLP Icon")]
+    [COMServerAssociation(AssociationType.FileExtension, ".blp")]
+    public class BlpThumbnailHandler : SharpIconHandler
+    {
+        public BlpThumbnailHandler()
+        {
+        }
+
+        protected override Icon GetIcon(bool smallIcon, uint iconSize)
+        {
+            if (!File.Exists(SelectedItemPath))
+                return new Icon(SystemIcons.Exclamation, new Size((int)iconSize, (int)iconSize));
+
+            using (var stream = File.OpenRead(SelectedItemPath))
+            using (var blpFile = new BlpFile(stream))
+            {
+                using (var bitmap = blpFile.GetBitmap(0))
+                {
+                    return Icon.FromHandle(bitmap.GetHicon());
+                }
+            }
+        }
+    }
+}
